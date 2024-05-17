aaf8000ed500ce57e05aa06c6e855e2573f0fb7b
CimimUxMaio <juanbiabdon@gmail.com>
2024-05-17 15:14:00 -0300

    Elimino archivos viejos + corrijo scripts/data.py

--- scripts/data.py
+++ scripts/data.py
@@ -1,3 +1,4 @@
+import functools
 import pandas as pd
 import numpy as np
 
@@ -76,6 +77,25 @@ def load_humidity_data() -> pd.DataFrame:
     return df
 
 
+def load_precipitation_data() -> pd.DataFrame:
+    # Read data from file
+    raw = pd.read_excel(
+        "./data/precipitaciones.xlsx",
+        sheet_name="MA_AX07",
+        header=[1, 2],
+        index_col=0,
+        nrows=13,  # Include "Total" row
+    )
+
+    # Rename columns
+    renames = {"Días": "Days"}
+    df = raw.rename(columns=renames)
+
+    # Replace "..." values with NaN
+    df.replace({"…": np.nan}, inplace=True)
+    return df
+
+
 def transposed_subcol(df: pd.DataFrame, subcol: str, value_name: str) -> pd.DataFrame:
     target = df.xs(subcol, level=1, axis=1)
     target.columns.rename("Year", inplace=True)
@@ -89,25 +109,58 @@ def transposed_subcol(df: pd.DataFrame, subcol: str, value_name: str) -> pd.Data
     return melted.reset_index().set_index(["Year", "Month"])
 
 
+def merge_analysis(*dfs: pd.DataFrame) -> pd.DataFrame:
+    def merge(df1, df2):
+        return df1.join(df2, on=["Year", "Month"], how="inner")
+
+    return functools.reduce(merge, dfs)
+
+
 def analysis_temperature() -> pd.DataFrame:
-    raw_temperature = load_temperature_data()
-    raw_temperature.columns = raw_temperature.columns.droplevel(-1)
-    return transposed_subcol(raw_temperature, "Mean", "Temp")
+    raw = load_temperature_data()
+
+    def merge_levels(header):
+        year, h1, h2 = header
+        return (year, h1 + h2, "")
+
+    raw.columns = raw.columns.map(merge_levels).droplevel(-1)
+
+    temperature_min_abs = transposed_subcol(raw, "MinAbsolute", "TempMinAbs")
+    temperature_max_abs = transposed_subcol(raw, "MaxAbsolute", "TempMaxAbs")
+    temperature_min_mean = transposed_subcol(raw, "MinMean", "TempMinMean")
+    temperature_max_mean = transposed_subcol(raw, "MaxMean", "TempMaxMean")
+    temperature_mean = transposed_subcol(raw, "MeanMean", "TempMean")
+
+    return merge_analysis(
+        temperature_min_abs,
+        temperature_max_abs,
+        temperature_min_mean,
+        temperature_max_mean,
+        temperature_mean,
+    )
 
 
 def analysis_humidity() -> pd.DataFrame:
-    raw_humidity = load_humidity_data()
+    raw = load_humidity_data()
 
-    humidity_min = transposed_subcol(raw_humidity, "Min", "HumMin")
-    humidity_max = transposed_subcol(raw_humidity, "Max", "HumMax")
-    humidity_mean = transposed_subcol(raw_humidity, "Mean", "HumMean")
+    humidity_min = transposed_subcol(raw, "Min", "HumMin")
+    humidity_max = transposed_subcol(raw, "Max", "HumMax")
+    humidity_mean = transposed_subcol(raw, "Mean", "HumMean")
 
-    return humidity_min.join(humidity_max, on=["Year", "Month"], how="inner").join(
-        humidity_mean, on=["Year", "Month"], how="inner"
-    )
+    return merge_analysis(humidity_min, humidity_max, humidity_mean)
+
+
+def analysis_precipitation() -> pd.DataFrame:
+    raw = load_precipitation_data().drop(index="Total")
+
+    precip_days = transposed_subcol(raw, "Days", "PrecipDays")
+    precip_mm = transposed_subcol(raw, "mm", "PrecipMm")
+
+    return merge_analysis(precip_days, precip_mm)
 
 
 def load_analysis_data() -> pd.DataFrame:
     temperature = analysis_temperature()
     humidity = analysis_humidity()
-    return temperature.join(humidity, on=["Year", "Month"], how="inner")
+    precipitations = analysis_precipitation()
+    return merge_analysis(temperature, humidity, precipitations)
